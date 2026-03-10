94e19168359bb7453438cf0c849eea61191221f9
Horea Nasalean <horea.nasalean@accesa.eu>
2026-03-10 09:57:48 +0200

    fix calculate triangle method

--- source/engine/model/meshutils.js
+++ source/engine/model/meshutils.js
@@ -1,55 +1,65 @@
-import { CrossVector3D, SubCoord3D } from '../geometry/coord3d.js';
-import { Transformation } from '../geometry/transformation.js';
+import { CrossVector3D, SubCoord3D } from "../geometry/coord3d.js";
+import { Transformation } from "../geometry/transformation.js";
 
-export function IsEmptyMesh (mesh)
-{
-    return mesh.LineCount () === 0 && mesh.TriangleCount () === 0;
+export function IsEmptyMesh(mesh) {
+  return mesh.LineCount() === 0 && mesh.TriangleCount() === 0;
 }
 
-export function CalculateTriangleNormal (v0, v1, v2)
-{
-    let v = SubCoord3D (v1, v0);
-    let w = SubCoord3D (v2, v0);
-    let normal = CrossVector3D (v, w);
-    normal.Normalize ();
-    return normal;
+export function CalculateTriangleNormal(v0, v1, v2) {
+  if (v0 === undefined || v1 === undefined || v2 === undefined) {
+    return { x: 0, y: 0, z: 1, Normalize() {} };
+  }
+
+  let v = SubCoord3D(v1, v0);
+  let w = SubCoord3D(v2, v0);
+  let normal = CrossVector3D(v, w);
+
+  if (
+    normal === undefined ||
+    normal.x === undefined ||
+    normal.y === undefined ||
+    normal.z === undefined
+  ) {
+    return { x: 0, y: 0, z: 1, Normalize() {} };
+  }
+
+  normal.Normalize();
+  return normal;
 }
 
-export function TransformMesh (mesh, transformation)
-{
-    if (transformation.IsIdentity ()) {
-        return;
-    }
+export function TransformMesh(mesh, transformation) {
+  if (transformation.IsIdentity()) {
+    return;
+  }
 
-    for (let i = 0; i < mesh.VertexCount (); i++) {
-        let vertex = mesh.GetVertex (i);
-        let transformed = transformation.TransformCoord3D (vertex);
-        vertex.x = transformed.x;
-        vertex.y = transformed.y;
-        vertex.z = transformed.z;
-    }
+  for (let i = 0; i < mesh.VertexCount(); i++) {
+    let vertex = mesh.GetVertex(i);
+    let transformed = transformation.TransformCoord3D(vertex);
+    vertex.x = transformed.x;
+    vertex.y = transformed.y;
+    vertex.z = transformed.z;
+  }
 
-    if (mesh.NormalCount () > 0) {
-        let normalMatrix = transformation.GetMatrix ().InvertTranspose ();
-        if (normalMatrix !== null) {
-            let normalTransformation = new Transformation (normalMatrix);
-            for (let i = 0; i < mesh.NormalCount (); i++) {
-                let normal = mesh.GetNormal (i);
-                let transformed = normalTransformation.TransformCoord3D (normal);
-                normal.x = transformed.x;
-                normal.y = transformed.y;
-                normal.z = transformed.z;
-            }
-        }
+  if (mesh.NormalCount() > 0) {
+    let normalMatrix = transformation.GetMatrix().InvertTranspose();
+    if (normalMatrix !== null) {
+      let normalTransformation = new Transformation(normalMatrix);
+      for (let i = 0; i < mesh.NormalCount(); i++) {
+        let normal = mesh.GetNormal(i);
+        let transformed = normalTransformation.TransformCoord3D(normal);
+        normal.x = transformed.x;
+        normal.y = transformed.y;
+        normal.z = transformed.z;
+      }
     }
+  }
 }
 
-export function FlipMeshTrianglesOrientation (mesh)
-{
-    for (let i = 0; i < mesh.TriangleCount (); i++) {
-        let triangle = mesh.GetTriangle (i);
-        let tmp = triangle.v1;
-        triangle.v1 = triangle.v2;
-        triangle.v2 = tmp;
-    }
+export function FlipMeshTrianglesOrientation(mesh) {
+  for (let i = 0; i < mesh.TriangleCount(); i++) {
+    let triangle = mesh.GetTriangle(i);
+    let tmp = triangle.v1;
+    triangle.v1 = triangle.v2;
+    triangle.v2 = tmp;
+  }
 }
